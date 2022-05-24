06a7168270381c8ef487d1c9553653336bef0212
cbarrios <lalobarrios16@yahoo.com>
2022-05-23 21:12:29 -0400

    master: (5) Implement the app (refactored)

--- app/src/main/java/com/lalosapps/lemonadeapp/MainActivity.kt
+++ app/src/main/java/com/lalosapps/lemonadeapp/MainActivity.kt
@@ -3,14 +3,14 @@ package com.lalosapps.lemonadeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,6 +21,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.lalosapps.lemonadeapp.ui.theme.LemonadeAppTheme
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,108 +41,86 @@ fun LemonApp() {
     // across recompositions).
     var currentStep by remember { mutableStateOf(1) }
 
-    // A surface container using the 'background' color from the theme
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
-    ) {
-        when (currentStep) {
-            1 -> StepOne { currentStep = 2 }
-            2 -> StepTwo { currentStep = 3 }
-            3 -> StepThree { currentStep = 4 }
-            4 -> StepFour { currentStep = 1 }
+    // Number of times the lemon needs to be squeezed to turn into a glass of lemonade
+    var squeezeCount by remember { mutableStateOf(0) }
+
+    // Snack
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    var job: Job? = remember { null }
+    val snackMessage = stringResource(id = R.string.keep_squeezing)
+
+    Scaffold(scaffoldState = scaffoldState) {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            when (currentStep) {
+                1 -> TextAndImageStep(
+                    text = R.string.lemon_tree_instructions,
+                    drawable = R.drawable.lemon_tree,
+                    contentDesc = R.string.lemon_tree_content_description
+                ) {
+                    currentStep = 2
+                    squeezeCount = (2..4).random()
+                }
+                2 -> TextAndImageStep(
+                    text = R.string.lemon_instructions,
+                    drawable = R.drawable.lemon_squeeze,
+                    contentDesc = R.string.lemon_content_description
+                ) {
+                    job?.cancel()
+                    if (--squeezeCount == 0) {
+                        currentStep = 3
+                    } else {
+                        job = scope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar(snackMessage)
+                        }
+                    }
+                }
+                3 -> TextAndImageStep(
+                    text = R.string.glass_of_lemonade_instructions,
+                    drawable = R.drawable.lemon_drink,
+                    contentDesc = R.string.glass_of_lemonade_content_description
+                ) {
+                    currentStep = 4
+                }
+                4 -> TextAndImageStep(
+                    text = R.string.empty_glass_instructions,
+                    drawable = R.drawable.lemon_restart,
+                    contentDesc = R.string.empty_glass_content_description
+                ) {
+                    currentStep = 1
+                }
+            }
         }
     }
+
 }
 
 @Composable
-fun StepOne(
-    onImageClicked: () -> Unit
+fun TextAndImageStep(
+    @StringRes text: Int,
+    @DrawableRes drawable: Int,
+    @StringRes contentDesc: Int,
+    onImageClick: () -> Unit
 ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(text = stringResource(id = R.string.lemon_tree_instructions), fontSize = 18.sp)
+        Text(text = stringResource(id = text), fontSize = 18.sp)
         Spacer(modifier = Modifier.height(16.dp))
         Image(
-            painter = painterResource(id = R.drawable.lemon_tree),
-            contentDescription = stringResource(id = R.string.lemon_tree_content_description),
+            painter = painterResource(id = drawable),
+            contentDescription = stringResource(id = contentDesc),
             modifier = Modifier
                 .wrapContentSize()
-                .clickable { onImageClicked() }
-                .border(2.dp, Color(105, 205, 216), RoundedCornerShape(4.dp))
-                .padding(16.dp)
-        )
-    }
-}
-
-@Composable
-fun StepTwo(
-    onImageClicked: () -> Unit
-) {
-    val squeeze = remember { (2..4).random() }
-    var squeezeCount by remember { mutableStateOf(0) }
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(text = stringResource(id = R.string.lemon_instructions), fontSize = 18.sp)
-        Spacer(modifier = Modifier.height(16.dp))
-        Image(
-            painter = painterResource(id = R.drawable.lemon_squeeze),
-            contentDescription = stringResource(id = R.string.lemon_content_description),
-            modifier = Modifier
-                .wrapContentSize()
-                .clickable { if (++squeezeCount == squeeze) onImageClicked() }
-                .border(2.dp, Color(105, 205, 216), RoundedCornerShape(4.dp))
-                .padding(16.dp)
-        )
-    }
-}
-
-@Composable
-fun StepThree(
-    onImageClicked: () -> Unit
-) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(text = stringResource(id = R.string.glass_of_lemonade_instructions), fontSize = 18.sp)
-        Spacer(modifier = Modifier.height(16.dp))
-        Image(
-            painter = painterResource(id = R.drawable.lemon_drink),
-            contentDescription = stringResource(id = R.string.glass_of_lemonade_content_description),
-            modifier = Modifier
-                .wrapContentSize()
-                .clickable { onImageClicked() }
-                .border(2.dp, Color(105, 205, 216), RoundedCornerShape(4.dp))
-                .padding(16.dp)
-        )
-    }
-}
-
-@Composable
-fun StepFour(
-    onImageClicked: () -> Unit
-) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(text = stringResource(id = R.string.empty_glass_instructions), fontSize = 18.sp)
-        Spacer(modifier = Modifier.height(16.dp))
-        Image(
-            painter = painterResource(id = R.drawable.lemon_restart),
-            contentDescription = stringResource(id = R.string.empty_glass_content_description),
-            modifier = Modifier
-                .wrapContentSize()
-                .clickable { onImageClicked() }
+                .clickable(onClick = onImageClick)
                 .border(2.dp, Color(105, 205, 216), RoundedCornerShape(4.dp))
                 .padding(16.dp)
         )
